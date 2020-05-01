cc51c104e0a90b3aa30a3f51184b0b7086499a1b
Jaiteg Singh <jaiteg1012@gmail.com>
2020-05-01 11:23:21 -0400

    yolo

--- database.py
+++ database.py
@@ -15,11 +15,11 @@ import time
 def coordinateOffset(latitude, longitude):
     radius = 6371000.0 #radius of earth 
     sign = secrets.choice(range(0, 2)) #used to determine sign of the offset
-    oLat = secrets.choice(range(3200, 8000)) #offset in metres (2 - 5 miles)
+    oLat = secrets.choice(range(500, 1200)) #offset in metres (700m - 1 mile)
     if (sign == 0):
         oLat = -1 * oLat
     sign = secrets.choice(range(0, 2)) #used to determine sign of the offset
-    oLong = secrets.choice(range(3200, 8000)) #offset in metres (2 - 5 miles)
+    oLong = secrets.choice(range(500, 1200)) #offset in metres (700m - 1 mile)
     if (sign == 0):
         oLong = -1 * oLong
     new_latitude  = latitude  + (oLat / radius) * (180 / math.pi)
@@ -489,7 +489,13 @@ def main(argv):
     
     user = entryInfo.entryInfo('Long Ho', 'lhho', 'lhho@princeton.edu', '7142602003', 'just a cali boy looking for kangaroos', 'Churchill Ave, Hobart TAS 7005, Australia')
     userTwo = entryInfo.entryInfo('Slim Jim', 'sjim', 'sjim@princeton.edu', '1234567', 'im a stick', '4000 Union Pacific Ave, Commerce, CA')
-    print(getMessages('jaitegs', 'billy'))
+    print(40.348600, -74.659300)
+
+
+    for i in range (0, 248):
+        coordinates = coordinateOffset(40.348600, -74.659300)
+        print(coordinates[0], coordinates[1])
+
    
 
 if __name__ == '__main__':
